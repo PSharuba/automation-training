ce2013d889b52668f10e29781163a336ab8d8c1f
PSharuba <mynameisfich@gmail.com>
2019-11-26 15:45:20 +0300

    update .gitignore

--- CleanCode/src/main/java/Planes/CustomPlane.java
+++
@@ -1,8 +0,0 @@
-package Planes;
-
-public class CustomPlane extends Plane {
-    public CustomPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity) {
-        super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
-    }
-
-}
