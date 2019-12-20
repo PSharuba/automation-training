cec49a8673a3685239772d3595c9152eb73f6184
PSharuba <mynameisfich@gmail.com>
2019-12-20 11:55:57 +0300

    add chrome options

--- Framework/src/test/java/by/mmf/sharubapv/driver/DriverSingleton.java
+++ Framework/src/test/java/by/mmf/sharubapv/driver/DriverSingleton.java
@@ -28,11 +28,13 @@ public class DriverSingleton {
                 }
                 default: {
                     WebDriverManager.chromedriver().setup();
-                    /*ChromeOptions option = new ChromeOptions();
-                    option.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
-                    DesiredCapabilities chrome = DesiredCapabilities.chrome();
-                    chrome.setJavascriptEnabled(true);
-                    option.setCapability(ChromeOptions.CAPABILITY, option);*/
+                    ChromeOptions options = new ChromeOptions();
+                    options.addArguments("start-maximized"); // open Browser in maximized mode
+                    options.addArguments("disable-infobars"); // disabling infobars
+                    options.addArguments("--disable-extensions"); // disabling extensions
+                    options.addArguments("--disable-gpu"); // applicable to windows os only
+                    options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+                    options.addArguments("--no-sandbox"); // Bypass OS security model
                     driver = new ChromeDriver();
                 }
             }
