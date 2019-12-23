f667e1bc180222d1c347e0a137f2794f6b3f7e22
PSharuba <mynameisfich@gmail.com>
2019-12-23 20:17:48 +0300

    fix chrome options

--- Framework/src/test/java/by/mmf/sharubapv/util/TestListener.java
+++ Framework/src/test/java/by/mmf/sharubapv/util/TestListener.java
@@ -10,6 +10,9 @@ import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.time.ZonedDateTime;
@@ -18,7 +21,8 @@ import java.time.format.DateTimeFormatter;
 
 public class TestListener implements ITestListener {
     private Logger log = LogManager.getRootLogger();
-    private final Logger logger= LogManager.getRootLogger();
+    private final Logger logger = LogManager.getRootLogger();
+    private final String SCREENSHOTS_PATH = ".//target/screenshots/";
 
     public void onTestStart(ITestResult iTestResult) {
 
@@ -45,25 +49,36 @@ public class TestListener implements ITestListener {
     }
 
     public void onFinish(ITestContext iTestContext) {
-logger.info("Test finished");
+        logger.info("Test finished");
     }
 
-    private void saveScreenshot(){
-        File screenCapture = ((TakesScreenshot) DriverSingleton
-                .getDriver())
-                .getScreenshotAs(OutputType.FILE);
+    private void saveScreenshot() {
+//        File screenCapture = ((TakesScreenshot) DriverSingleton
+//                .getDriver())
+//                .getScreenshotAs(OutputType.FILE);
+//        try {
+//            FileUtils.copyFile(screenCapture, new File(
+//                    ".//target/screenshots/"
+//                            + getCurrentTimeAsString() +
+//                            ".png"));
+//        } catch (IOException e) {
+//            log.error("Failed to save screenshot: " + e.getLocalizedMessage());
+//        }
         try {
-            FileUtils.copyFile(screenCapture, new File(
-                    ".//target/screenshots/"
-                    + getCurrentTimeAsString() +
-                    ".png"));
-        } catch (IOException e) {
-            log.error("Failed to save screenshot: " + e.getLocalizedMessage());
+            Thread.sleep(120);
+            Robot r = new Robot();
+            Rectangle capture =
+                    new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+            BufferedImage Image = r.createScreenCapture(capture);
+            ImageIO.write(Image, "jpg", new File(SCREENSHOTS_PATH));
+            System.out.println("Screenshot saved");
+        } catch (AWTException | IOException | InterruptedException ex) {
+            System.out.println("Error taking screenshot: " + ex);
         }
     }
 
-    private String getCurrentTimeAsString(){
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern( "uuuu-MM-dd_HH-mm-ss" );
+    private String getCurrentTimeAsString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("uuuu-MM-dd_HH-mm-ss");
         return ZonedDateTime.now().format(formatter);
     }
 }
